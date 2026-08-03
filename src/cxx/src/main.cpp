ab674ce11324a59f3aea2e1b71a1c43912ace118
ohmycode-cn <ohcode@163.com>
2026-08-03 17:13:17 +0800

    Remove miniterm stubs and refactor main entry point
    - Remove miniterm module (Terminal, WinTerm) stubs - deferred to later
    - Disable test_c_main() and test_main() calls in favor of Interaction REPL
    - Refactor main(): use if-with-initializer for interaction->run() result
    - Change main(void) to main() per C++ convention

--- src/cxx/src/main.cpp
+++ src/cxx/src/main.cpp
@@ -1,10 +1,11 @@
-#include "test/ruac_c_test_main.h"
-#include "test/ruac_test_main.hpp"
+// #include "test/ruac_c_test_main.h"
+// #include "test/ruac_test_main.hpp"
+
 #include "ruac_interaction.hpp"
 
-int main(void) {
-    test_c_main();
-    ruac::test::test_main();
+int main() {
+    // test_c_main();
+    // ruac::test::test_main();
 
     auto interaction = new ruac::Interaction();
     interaction->init({
@@ -13,8 +14,8 @@ int main(void) {
         .m_enable_bf = false,
         .m_enable_dp = true,
     });
-    auto end = interaction->run();
-    if (end && nullptr != interaction) {
+
+    if (const auto end = interaction->run(); end && nullptr != interaction) {
         delete interaction;
         interaction = nullptr;
     }
